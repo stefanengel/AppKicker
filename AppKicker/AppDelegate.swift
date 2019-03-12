f2a43d309b9d94445841154a8f17b3fb4d96796d
Stefan Engel <stefan.e82@gmx.de>
2019-03-12 17:18:14 +0100

    Initial Commit

---
+++ AppKicker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AppKicker
+//
+//  Created by Stefan Engel on 12.03.19.
+//  Copyright © 2019 Stefan Engel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ AppKicker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AppKicker
+//
+//  Created by Stefan Engel on 12.03.19.
+//  Copyright © 2019 Stefan Engel. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
