9da967cbaf9657c5d14eb448a9ec5ba074651f56
Stefan Engel <stefan.e82@gmx.de>
2019-03-14 18:49:19 +0100

    feature: Add checkboxes for conditions

--- AppKicker/ViewController.swift
+++ AppKicker/ViewController.swift
@@ -13,10 +13,24 @@ class ViewController: NSViewController {
 	@IBOutlet weak var applicationLabel: NSTextField!
 	@IBOutlet weak var scriptLabel: NSTextField!
 	@IBOutlet weak var intervalLabel: NSTextField!
+	@IBOutlet weak var connectionLabel: NSTextField!
+	@IBOutlet weak var conditionLabel: NSTextField!
 	@IBOutlet weak var applicationPopup: NSPopUpButton!
 	@IBOutlet weak var scriptTextField: NSTextField!
 	@IBOutlet weak var intervalTextField: NSTextField!
+	@IBOutlet weak var connectionTextField: NSTextField!
+	@IBOutlet weak var enableScriptCheckbox: NSButton!
+	@IBOutlet weak var enableConnectionCheckbox: NSButton!
 	
+	@IBAction func enableScriptCheckboxPressed(_ sender: Any) {
+		scriptTextField.isEnabled = enableScriptCheckbox.state == .on
+		scriptLabel.alphaValue = enableScriptCheckbox.state == .on ? 1.0 : 0.5
+	}
+	@IBAction func enableConnectionCheckboxPressed(_ sender: Any) {
+		connectionTextField.isEnabled = enableConnectionCheckbox.state == .on
+		connectionLabel.alphaValue = enableConnectionCheckbox.state == .on ? 1.0 : 0.5
+	}
+
 	@IBAction func startButtonPressed(_ sender: Any) {
 		guard let interval = Int(intervalTextField.stringValue) else {
 			return
@@ -34,6 +48,11 @@ class ViewController: NSViewController {
 		scriptLabel.stringValue = "Script:"
 		intervalLabel.stringValue = "Interval (seconds):"
 		intervalTextField.formatter = IntegerValueFormatter()
+		connectionLabel.stringValue = "Ensure connection to:"
+		conditionLabel.stringValue = "Conditions"
+
+		enableScriptCheckbox.title = "Enabled"
+		enableConnectionCheckbox.title = "Enabled"
 
 		for app in NSWorkspace.shared.runningApplications {
 			applicationPopup.addItem(withTitle: app.executableURL!.lastPathComponent)
