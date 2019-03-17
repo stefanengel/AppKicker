2f585f4d8f30122383b1bc9355e2d4f39328116c
Stefan Engel <stefan.e82@gmx.de>
2019-03-17 16:26:50 +0100

    feature: Make app restarter more robust

--- AppKicker/Control/AppRestarter.swift
+++ AppKicker/Control/AppRestarter.swift
@@ -8,20 +8,38 @@
 
 import Cocoa
 
-struct AppRestarter {
-
+class AppRestarter {
+	static var currentApp: NSRunningApplication?
 }
 
 // MARK: - Restarting an app
 extension AppRestarter {
 	static func restart(app executableUrl: URL) {
+		guard currentApp == nil else {
+			Logger.shared.log(message: "App termination already in progress, ignoring new request")
+			return
+		}
+
 		for app in NSWorkspace.shared.runningApplications {
 			if app.executableURL! == executableUrl {
+				currentApp = app
+				Logger.shared.log(message: "Calling forceTerminate")
 				app.forceTerminate()
+
+				Timer.scheduledTimer(timeInterval: TimeInterval(5), target: self, selector: #selector(start), userInfo: nil, repeats: false)
 				break
 			}
 		}
+	}
 
-		NSWorkspace.shared.launchApplication(executableUrl.path)
+	@objc static func start() {
+		guard let url = currentApp?.executableURL else {
+			currentApp = nil
+			return
+		}
+
+		currentApp = nil
+		Logger.shared.log(message: "Calling launchApplication")
+		NSWorkspace.shared.launchApplication(url.path)
 	}
 }
